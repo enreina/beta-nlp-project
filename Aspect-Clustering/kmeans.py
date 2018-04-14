c971f46b07b98eec69ffca6b0282de13ce246d0b
Enreina Annisa Rizkiasri <enreina@gmail.com>
2018-04-14 22:27:53 +0200

    read training data for kmeans

--- Aspect-Clustering/kmeans.py
+++ Aspect-Clustering/kmeans.py
@@ -5,9 +5,10 @@ import numpy as np
 import pandas as pd
 
 #X = np.array([[1, 2], [1, 4], [1, 0], [4, 2], [4, 4], [4, 0]])
-X = pd.read_csv('C:\\Users\\chitra\\Desktop\\TU-Delft\\Information Retrival\\movie-reviews\\test1.csv')
-df1 = pd.DataFrame(X)
-centroids, labels, inertia = cluster.k_means (X, n_clusters = 4)
+X = pd.read_csv("../word2vec/output/word_vectors_training.csv",header=None)
+vectors = X.iloc[:,1:] # this is the vector representation, from column 1 till the end
+words = X[X.columns[0]] # this is the first column, the real "words"
+centroids, labels, inertia = cluster.k_means (vectors, n_clusters = 4)
 
 #kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
 np.savetxt('C:\\Users\\chitra\\Desktop\\TU-Delft\\Information Retrival\\movie-reviews\\label.txt',labels,fmt='%d')
