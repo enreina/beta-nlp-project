a03235dd88c1531a2cd018b7a9cc2b0197c084b8
chitra balasubramanian <chitra2793@gmail.com>
2018-04-15 19:29:24 +0200

    Update kmeans.py

--- Aspect-Clustering/kmeans.py
+++ Aspect-Clustering/kmeans.py
@@ -3,15 +3,20 @@ from sklearn.cluster import KMeans
 from sklearn import cluster
 import numpy as np
 import pandas as pd
+import sent
 
 #X = np.array([[1, 2], [1, 4], [1, 0], [4, 2], [4, 4], [4, 0]])
-X = pd.read_csv("../word2vec/output/word_vectors_training.csv",header=None)
+X = pd.read_csv('C:\\Users\\chitra\\Desktop\\TU-Delft\\Information Retrival\\movie-reviews\\word_vectors_training.csv')
 vectors = X.iloc[:,1:] # this is the vector representation, from column 1 till the end
-words = X[X.columns[0]] # this is the first column, the real "words"
-centroids, labels, inertia = cluster.k_means (vectors, n_clusters = 4)
+words = X[X.columns[0]]# this is the first column, the real "words"
+value = [0]*10 #trying for the first 10 samples, can increase the array size
+for i in range(10):
+    value[i] = sent.word_similarity(words[i], words[i+1], model="google-news")
+print value
+value = np.asarray(value)#convert the list into array
 
-# and then we can combine labels and words
-result = pd.DataFrame({"Word": words, "Label":labels})
+#df1 = pd.DataFrame(X)
+centroids, labels, inertia = cluster.k_means(value, n_clusters = 4)
 
 #kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
 np.savetxt('C:\\Users\\chitra\\Desktop\\TU-Delft\\Information Retrival\\movie-reviews\\label.txt',labels,fmt='%d')
